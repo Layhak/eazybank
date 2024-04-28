cd8573433ca358cbfb19234ac7f9f59108b4274b
LAYHAK <layhak.garu@gmail.com>
2024-04-28 21:14:54 +0700

    first init

---
+++ src/main/java/com/spring/secruity/example/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.spring.secruity.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to spring application with security";
+    }
+}
---
+++ src/test/java/com/spring/secruity/example/SpringSecurityLesson1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.secruity.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLesson1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
